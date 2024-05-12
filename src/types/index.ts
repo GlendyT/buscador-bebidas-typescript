d18ca436bb2a667297e92a50c1aa64f40f2816c8
GlendyT <glendytuyuc@gmail.com>
2024-05-11 19:03:16 -0600

    Agregar modal para ver los ingredientes e iniciar con el componente y slice de Favoritos

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
-import { CategoriesApiResponseSchema, DrinksAPIResponse, DrinkAPIResponse SearchFilterSchema } from "../utils/recipes-schema";
+import { CategoriesApiResponseSchema, DrinksAPIResponse, DrinkAPIResponse, SearchFilterSchema,RecipeAPIResponseSchema } from "../utils/recipes-schema";
 
 export type Categories = z.infer<typeof CategoriesApiResponseSchema>;
 export type SearchFilter = z.infer<typeof SearchFilterSchema>
 export type Drinks = z.infer<typeof DrinksAPIResponse>
 export type Drink = z.infer<typeof DrinkAPIResponse>
+export type Recipe = z.infer<typeof RecipeAPIResponseSchema> 
